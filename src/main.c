56aa72613bd6dc4e8bc7ff08a0470efed737f8e9
mohinsert <mohinsert@gmail.com>
2014-06-27 00:01:40 +0300

    little rearrangement of WinMain function

--- src/main.c
+++ src/main.c
@@ -25,25 +25,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int CALLBACK WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	MSG msg;
-	WNDCLASSEX wx;
-
-	wx.cbSize        = sizeof(WNDCLASSEX);
-    wx.style         = 0;
-    wx.lpfnWndProc   = WndProc;
-    wx.cbClsExtra    = 0;
-    wx.cbWndExtra    = 0;
-    wx.hInstance     = hInstance;
-    wx.hIcon         = 0;
-    wx.hCursor       = 0;
-    wx.hbrBackground = 0;
-    wx.lpszMenuName  = 0;
-    wx.lpszClassName = TEXT("flatwm");
-    wx.hIconSm       = 0;
+	WNDCLASSEX wx = {
+		.cbSize        = sizeof(WNDCLASSEX),
+	    .style         = 0,
+	    .lpfnWndProc   = WndProc,
+	    .cbClsExtra    = 0,
+	    .cbWndExtra    = 0,
+	    .hInstance     = hInstance,
+	    .hIcon         = 0,
+	    .hCursor       = 0,
+	    .hbrBackground = 0,
+	    .lpszMenuName  = 0,
+	    .lpszClassName = TEXT("flatwm"),
+	    .hIconSm       = 0
+	};
 
 	RegisterClassEx(&wx);
 	CreateWindowEx(0, wx.lpszClassName, TEXT("Dummy"), 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, NULL, NULL); 	
 
+	MSG msg;
 	while(GetMessage(&msg, NULL, 0, 0)) {
 		DispatchMessage(&msg);
 	}
